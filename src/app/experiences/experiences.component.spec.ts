7b9a8be03381a196f5a05325db37e9e60754b8f7
Damien Rubio <rubio.damien@gmail.com>
2024-10-08 09:15:35 +0200

    feat(ci): automate release from main

--- src/app/experiences/experiences.component.spec.ts
+++ src/app/experiences/experiences.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ExperiencesComponent } from './experiences.component';
 
@@ -6,7 +6,7 @@ describe('ExperiencesComponent', () => {
   let component: ExperiencesComponent;
   let fixture: ComponentFixture<ExperiencesComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ExperiencesComponent ]
     })
--- src/app/experiences/scholarship/scholarship.component.spec.ts
+++ src/app/experiences/scholarship/scholarship.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ScholarshipComponent } from './scholarship.component';
 
@@ -6,7 +6,7 @@ describe('ScholarshipComponent', () => {
   let component: ScholarshipComponent;
   let fixture: ComponentFixture<ScholarshipComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ScholarshipComponent ]
     })
