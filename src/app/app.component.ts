695f414a5278a2fed6ca3021e43f5a556a2b325c
Damien Rubio <bonjour@damienrubio.com>
2025-01-23 16:33:28 +0100

    feat: add Skills and Experiences components to app and experiences modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
+import { SkillsComponent } from './skills/skills.component';
+import { ExperiencesComponent } from './experiences/experiences.component';
 
 @Component({
+  imports: [ExperiencesComponent, SkillsComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
